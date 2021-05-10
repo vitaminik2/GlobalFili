b36752d20857cc3e23eca5a039f9a6524c305059
vitaminik2 <78158295+vitaminik2@users.noreply.github.com>
2021-05-10 17:14:11 +0300

    Add files via upload

---
+++ Открытый урок333.py
@@ -0,0 +1,109 @@
+from tkinter import *
+
+ 
+def button_press():
+    print("Правильно")
+    
+tk = Tk()
+a = Button(tk, text= "4", background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+a.pack()
+tk.title("2+2=")
+
+def button_press():
+    print("Не правильно")
+
+
+tk.geometry("300x250")
+ 
+a = Button(tk, text="6",background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+a.pack()
+
+##########################################################################################################################################
+##########################################################################################################################################
+def button_press():
+    print("Правильно")
+
+tk = Tk()   
+
+b = Button(tk, text= "7", background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+b.pack()
+tk.title("24-17=")
+
+def button_press():
+    print("Не правильно")
+
+
+tk.geometry("300x250")
+ 
+b = Button(tk, text="4",background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+b.pack()
+##########################################################################################################################################
+##########################################################################################################################################
+def button_press():
+    print("Не Правильно")
+
+tk = Tk()   
+
+c = Button(tk, text= "9", background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+c.pack()
+tk.title("Сколько ног у осьминога?")
+
+def button_press():
+    print("Правильно")
+
+
+tk.geometry("300x250")
+ 
+c = Button(tk, text="8",background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+c.pack()
+##########################################################################################################################################
+##########################################################################################################################################
+def button_press():
+    print("Правильно")
+
+tk = Tk()   
+
+d = Button(tk, text= "8", background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+d.pack()
+tk.title("64/8=")
+def button_press():
+    print("Не правильно")
+
+
+tk.geometry("300x250")
+ 
+d = Button(tk, text="6",background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+d.pack()
+##########################################################################################################################################
+##########################################################################################################################################
+def button_press():
+    print("Правильно")
+
+tk = Tk()   
+
+y = Button(tk, text= "8", background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+y.pack()
+tk.title("сколько планет в солнечной системе?")
+def button_press():
+    print("Правильно")
+
+
+tk.geometry("300x250")
+ 
+y = Button(tk, text="9",background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+y.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
